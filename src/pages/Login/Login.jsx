9cf84c07d4ed8b6e926e7e16d03f6cbed5b6fff8
shaeel123 <mohammedshaeel564@gmail.com>
2026-06-11 13:29:03 +0530

    change video

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 import ReactDOM from 'react-dom'
 
-const VIDEO_URL = 'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260506_031045_0e1165dd-ab48-46e3-ad3d-5fe77f217647.mp4'
+const VIDEO_URL = 'https://d8j0ntlcm91z4.cloudfront.net/user_38xzZboKViGWJOttwIXH07lWA1P/hf_20260314_131748_f2ca2a28-fed7-44c8-b9a9-bd9acdd5ec31.mp4'
 const TERMS_TEXT = `Terms and Conditions for Chat App
 Last updated: May 23, 2026
 
